680a862be8fb298822998fa00b8ca426ceaa9323
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-02-15 08:09:20 -0800

    Comple functionality

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ $(document).ready(function () {
   var keyCounter = 0;
 
 
-  //adds second emotion box when an emotion is selected
-
-
   //initializes workflow
   $(".btn-start").on("click", function () {
+    $("input[type='text']").val("");
     $(divs).hide('slow');
     $(".container-top").hide('slow');
     $(".summarize").show('slow');
@@ -28,7 +26,6 @@ $(document).ready(function () {
   //controls back and next buttons to hide and show divs as well as update next summary
   $(".btn-next").on("click", function (e) {
     let parent = e.currentTarget.parentElement.parentElement;
-    console.log(e);
     let next = $(parent).next();
 
     let val = getInputVal(parent);
@@ -50,7 +47,7 @@ $(document).ready(function () {
     keyCounter--;
   })
 
-  $(".btn-finish").on("click", function(e) {
+  $(".btn-finish").on("click", function (e) {
     window.alert("You've seen a difference of " + ($(".slider-second").val() - localStorage.getItem(1) + "%"))
   })
 
@@ -80,7 +77,7 @@ $(document).ready(function () {
         }).get();
         returnDiv.push("<span><u>Thought Distortions:</u></span><div><span>" + thoughts + "</br>");
       } else if (summary[i] === "Modified Thoughts") {
-        returnDiv.push("<div><span><u>" + summary[i] + ":</u></br>" + localStorage.getItem(i) + "</span></div></br><div><span><h5>You said you felt:</h5>" + val(".emotion-first") + ": " + val(".slider-first") + "%</span></div></br><span>How do you feel now?</span></br><input type='range' class='input slider-emotion slider-second' name='slider-emotion' min='0' max='100' step='10' value='0'><span>0%</span>")
+        returnDiv.push("<div><span><u>" + summary[i] + ":</u></br>" + localStorage.getItem(i) + "</span></div></br><div><span><h5 class='key'>You said you felt:</h5>" + val(".emotion-first") + ": " + val(".slider-first") + "%</span></div></br><h3>How do you feel now?</h3></br><input type='range' class='input slider-emotion slider-second' name='slider-emotion' min='0' max='100' step='10' value='0'><span>0%</span>")
       } else {
         returnDiv.push("<div><span><u>" + summary[i] + ":</u></br>" + localStorage.getItem(i) + "</span></div></br>")
       }
@@ -89,54 +86,7 @@ $(document).ready(function () {
     return returnDiv;
   }
 
-  //gets checkbox values
 
 
 
 });
-
-
-
-
-//getter setter stuff
-/*
- $('.btn-add').on('click', function (e) {
-    console.log(e);
-    var keyData = makeKey();
-    var valueData = $('.input-value').val();
-    // write to db
-    localStorage.setItem(keyData, limitValRange(valueData));
-    // read from db
-
-    // this only displays the last one? might want to switch to html
-    // and append a div
-    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-    // if you use backticks ` you can use ${templateLiterals}
-    // TODO make this vars make sense across the app
-    $('.input-key').val('');
-    $('.input-value').val('');
-  });
-
-
-  // update db
-  // need to expand when  more than 1 item is added
-
-  // delete item
-  $('.container-data').on('click', '.display-data-item', function (e) {
-    console.log(e.currentTarget.dataset.keyvalue);
-    var keyData = e.currentTarget.dataset.keyvalue;
-    localStorage.removeItem(keyData);
-    $('.container-data').text('');
-  });
-  // delete all?
-  $('.btn-clear').click(function () {
-    localStorage.clear();
-    $('.container-data').text('');
-  });
-
-  var makeKey = function () {
-  var date = new Date();
-  let time = date.getTime()
-  return date.toDateString() + " " + time;
-}
-  */
